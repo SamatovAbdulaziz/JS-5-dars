1f68b8cac29229762550b897b11fc908a48b376f
aqdtjjksytjksfyksk <142991426+SamatovAbdulaziz@users.noreply.github.com>
2023-08-26 14:01:37 +0500

    JS 5-dars
    26-guruh o'quvchisi

---
+++ main.js
@@ -0,0 +1,89 @@
+let users = [
+    {
+        login: "supra",
+        password: "12345"
+    }
+];
+
+$(document).ready(function (){
+    $('.btn-success').click(function (){
+      let login = $('#login').val();
+      let password = $('#password').val();
+      let num = false;
+
+      if (users.length !=0){
+          users.forEach(function (item){
+              if (login ==item.login){
+                  if (password == item.password){
+                      num = true;
+                      alert("Siz tizimga kirdingiz!");
+                  }
+                  else{
+                      alert("Parolingiz noto'g'ri!");
+                  }
+              }
+          });
+      }
+      else{
+          alert("Siz hali ro'yhatdan o'tmagansiz. Iltimos, ro'yhatdan o'ting!")
+      }
+
+    });
+
+    $('.btn-link').click(function (){
+        let login = $('#login').val();
+        let password =$('#password').val();
+        let num = false;
+
+        if (login == ''){
+            alert("Login qatorini toldiring! ");
+        }
+        else if (password == ''){
+            alert("Parol qatorini to'ldiring!");
+        }
+        else{
+            if (users.length !=0){
+                users.forEach(function (item ){
+                    if (login == item.login){
+                        num = true
+                    }
+                });
+                if (num){
+                    alert("Bunday foydalanuvchi mavjud!");
+                }
+                else{
+                    alert("Siz ro'yxatdan o'tdingiz!")
+                    users.push({login: login,password: password});
+                }
+            }
+            else{
+               alert("Siz hali ro'yhatdan o'tmagansiz. Iltimos, ro'yhatdan o'ting!");
+            }
+        }
+
+    });
+
+    $('btn-light').on("mousedown", function (){
+        let attr = $('#password').attr("type");
+        $('#password').attr("type","text");
+        $(".fa").toggleClass('fa-eye fa-eye-slash');
+        $("#password").css('border-color','red');
+    });
+
+    $('.btn-light').on("mouseup", function (){
+        let attr = $('#password').attr("type");
+        $('#password').attr("type","password");
+        $(".fa").toggleClass('fa-eye fa-eye-slash');
+        $('#password').css('border-color','blue');
+    });
+});
+
+
+
+
+
+
+
+
+
+
